d2772980f3a502a7feb91058a430911fb04a1fc8
Venkatesh.K.S <vengat417@gmail.com>
2025-01-16 16:27:40 +0530

    Merge result 2

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ console.log("Version 1");
 console.log("Version 2");
 console.log("Version 3");
 console.log("Version 4");
-<<<<<<< HEAD
 console.log("Bug Fixed");
-=======
 console.log("Feature 1");
 console.log("Feature 2");
 console.log("Feature 3");
->>>>>>> feature1
+console.log("Merge result worked");
